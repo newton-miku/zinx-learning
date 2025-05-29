81ca8807289ffcc8caa44959e334d2d0b2a8d4ca
newton_miku <29966140+newton-miku@users.noreply.github.com>
2025-05-29 15:11:48 +0800

    v0.2实现了Connection模块的封装

--- src/znet/server.go
+++ src/znet/server.go
@@ -1,8 +1,8 @@
 package znet
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"zinx/ziface"
@@ -10,53 +10,46 @@ import (
 
 type Server struct {
 	Name      string
-	Addr      string
 	IP        string
 	Port      int
 	IPVersion string
 }
 
+func EchoHandlerFunc(conn *net.TCPConn, data []byte, datalen int) error {
+	log.Printf("[%s] recv from client: %s\n", conn.RemoteAddr().String(), string(data[:datalen]))
+	_, err := conn.Write(data[:datalen])
+	if err != nil {
+		log.Printf("[%s]send data err: %v\n", conn.RemoteAddr().String(), err)
+		return errors.New("EchoHandlerFunc send data err")
+	}
+	return nil
+}
 func (s *Server) Start() {
-	log.Printf("[%s] Listening and accepting at %s\n", s.Name, s.Addr)
-	_, err := net.ResolveTCPAddr(s.IPVersion, s.Addr)
+	log.Printf("[%s] Listening and accepting at %s Port %d\n", s.Name, s.IP, s.Port)
+	Addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
 	//检查Addr是否有误
 	if err != nil {
 		log.Printf("[%s]start Zinx server failed, ip ver:%s\t addr err: %v\n", s.Name, s.IPVersion, err)
 		return
 	}
-	listener, err := net.Listen(s.IPVersion, s.Addr)
+	listener, err := net.ListenTCP(s.IPVersion, Addr)
 	if err != nil {
 		log.Printf("[%s]start Zinx server failed, ip ver:%s\t listen err: %v\n", s.Name, s.IPVersion, err)
 		return
 	}
-	log.Printf("[%s]Zinx server start successfully, now listening at %s\n", s.Name, s.Addr)
+	log.Printf("[%s]Zinx server start successfully, now listening at %s Port %d\n", s.Name, s.IP, s.Port)
 
+	connID := 0
 	for {
 		//服务器开始接受连接
-		conn, err := listener.Accept()
+		conn, err := listener.AcceptTCP()
 		if err != nil {
 			log.Printf("[%s]accept err: %v\n", s.Name, err)
 			continue
 		}
-		go func() {
-			defer conn.Close()
-			str := make([]byte, 1024)
-			for {
-				n, err := conn.Read(str)
-				if err != nil && err != io.EOF {
-					log.Printf("[%s]Client %s read err: %v\n", s.Name, conn.LocalAddr().String(), err)
-					return
-				}
-				if n == 0 {
-					log.Printf("[%s]Client %s read 0 bytes,May Dead\n", s.Name, conn.LocalAddr().String())
-					return
-				}
-				if _, err := conn.Write(str[:n]); err != nil {
-					log.Printf("[%s]Client %s write err: %v\n", s.Name, conn.LocalAddr().String(), err)
-					return
-				}
-			}
-		}()
+		dealConn := NewConnection(conn, uint(connID), EchoHandlerFunc)
+		connID++
+		dealConn.Start()
 	}
 }
 
@@ -71,7 +64,6 @@ func (s *Server) Init() {
 func NewServer(name string, ip string, port int) ziface.IServer {
 	return &Server{
 		Name:      name,
-		Addr:      net.JoinHostPort(ip, fmt.Sprint(port)),
 		IP:        ip,
 		Port:      port,
 		IPVersion: "tcp",
